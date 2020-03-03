01855198729c05751dc37301159e968487f2d29a
dcorn068 <dcorn068@users.noreply.github.com>
2020-03-02 22:08:26 -0500

    encode decode and store state in uri

--- src/App.js
+++ src/App.js
@@ -3,13 +3,19 @@ import React, { useState } from "react";
 import { ControlledEditor } from "@monaco-editor/react";
 import styled from "styled-components/macro";
 import { Switch, Button } from "@material-ui/core";
+import useReactRouter from "use-react-router";
+// import qs from "query-string";
 
+const CONTROLS_HEIGHT = 42;
 const AppStyles = styled.div`
+  * {
+    box-sizing: border-box;
+  }
   .controls {
     position: relative;
-    height: 42px;
+    height: ${CONTROLS_HEIGHT}px;
     display: flex;
-    margin: 0.5em;
+    padding: 0.5em;
     align-items: center;
     justify-content: space-around;
   }
@@ -20,11 +26,16 @@ const AppStyles = styled.div`
 `;
 
 export default () => {
-  const [value, setValue] = useState("// try to write e%v%a%l somewhere 😈 \n");
+  // const parsed = qs.parse(window.location.search);
+  const { location, history } = useReactRouter();
+  // console.log("⚡🚨: parsed", parsed);
+  const [value, setValue] = useState(decodeURI(location.search.slice(1))); // slice off the question mark
+  console.log("⚡🚨: location", location.search);
   const [isLightTheme, setIsLightTheme] = useState(false);
 
   const handleEditorChange = (ev, value) => {
     setValue(value);
+    history.push(`/?${encodeURI(value)}`);
   };
   const handleBuild = () => {};
 
@@ -43,9 +54,10 @@ export default () => {
       <ControlledEditor
         value={value}
         onChange={handleEditorChange}
-        height="90vh"
+        height={`calc(100vh - ${CONTROLS_HEIGHT}px)`}
         language="markdown"
         theme={isLightTheme ? "light" : "dark"}
+        options={{ wordWrap: "on" }}
       />
     </AppStyles>
   );
